51d45222d8996c0527a6c9d68863c03564cd35ce
MIC-P\s_jang <jang.seongyu@mic-p.com>
2024-06-03 14:15:18 +0900

    update

--- practice_pandas.py
+++ practice_pandas.py
@@ -1,16 +1,6 @@
 import pandas as pd
+df = pd.read_csv("./titanic.csv")
 
-# series_data = [10,15,20,25]
-# ser = pd.Series(series_data)
-# print(ser)
-
-dataframe_data = ([
-    [100, "a", True],
-    [200, "b", False],
-    [150, "d", False],
-    [550, "c", True]
-])
-
-df = pd.DataFrame(dataframe_data)
-df.index=["01","02","03","04"]
-df.columns=["A","B","C"]
+for row in zip(df["Age"],["Pclass"]):
+    print(row)
+    
